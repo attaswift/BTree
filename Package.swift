f29e813c6242b6f6d3d274999f229d60dd972e28
Karoly Lorentey <karoly@lorentey.hu>
2015-12-15 17:12:11 +0100

    Add package description for the Swift package manager.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  TreeCollections
+//
+//  Created by Károly Lőrentey on 2015-12-15.
+//  Copyright © 2015 Károly Lőrentey. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "TreeCollections",
+    dependencies: []
+)
