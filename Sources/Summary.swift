69c9f8fadbaad2746a75d02d5efa382302f7e263
Karoly Lorentey <karoly@lorentey.hu>
2015-12-20 15:11:07 +0100

    Add += operator for summaries.

--- Sources/Summary.swift
+++ Sources/Summary.swift
@@ -29,31 +29,50 @@ public protocol SummaryProtocol: Equatable {
 @warn_unused_result
 public func + <Summary: SummaryProtocol>(a: Summary?, b: Summary?) -> Summary {
     switch (a, b) {
-    case (nil, nil): return Summary()
-    case (nil, .Some(let b)): return b
-    case (.Some(let a), nil): return a
-    case (.Some(let a), .Some(let b)): return Summary(a, b)
+    case (nil, nil):
+        return Summary()
+    case (nil, .Some(let b)):
+        return b
+    case (.Some(let a), nil):
+        return a
+    case (.Some(let a), .Some(let b)):
+        return Summary(a, b)
     }
 }
 
 @warn_unused_result
 public func + <Summary: SummaryProtocol>(a: Summary.Item?, b: Summary?) -> Summary {
     switch (a, b) {
-    case (nil, nil): return Summary()
-    case (nil, .Some(let b)): return b
-    case (.Some(let a), nil): return Summary(a)
-    case (.Some(let a), .Some(let b)): return Summary(Summary(a), b)
+    case (nil, nil):
+        return Summary()
+    case (nil, .Some(let b)):
+        return b
+    case (.Some(let a), nil):
+        return Summary(a)
+    case (.Some(let a), .Some(let b)):
+        return Summary(Summary(a), b)
     }
 }
 
 @warn_unused_result
 public func + <Summary: SummaryProtocol>(a: Summary?, b: Summary.Item?) -> Summary {
     switch (a, b) {
-    case (nil, nil): return Summary()
-    case (nil, .Some(let b)): return Summary(b)
-    case (.Some(let a), nil): return a
-    case (.Some(let a), .Some(let b)): return Summary(a, Summary(b))
+    case (nil, nil):
+        return Summary()
+    case (nil, .Some(let b)):
+        return Summary(b)
+    case (.Some(let a), nil):
+        return a
+    case (.Some(let a), .Some(let b)):
+        return Summary(a, Summary(b))
     }
+}
+
+public func += <S: SummaryProtocol>(inout left: S, right: S.Item?) {
+    left = left + right
+}
+public func += <S: SummaryProtocol>(inout left: S, right: S?) {
+    left = left + right
 }
 
 /// The empty summary is simply a summary that is a constant empty value. 
