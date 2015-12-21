d1ee272b5d0094d8140448a3a4e8fd293fbb75ba
Karoly Lorentey <karoly@lorentey.hu>
2015-12-21 20:24:20 +0100

    Make SortedArray public.

---
+++ PerformanceTests/Duration.swift
@@ -0,0 +1,9 @@
+//
+//  Duration.swift
+//  TreeCollections
+//
+//  Created by Károly Lőrentey on 2015-12-20.
+//  Copyright © 2015 Károly Lőrentey. All rights reserved.
+//
+
+import Foundation
--- Sources/SortedArray.swift
+++ Sources/SortedArray.swift
@@ -9,30 +9,34 @@
 import Foundation
 
 /// A sorted associative container with copy-on-write value semantics that uses a sorted array as the backing store.
-internal struct SortedArray<Key: Comparable, Value>: SortedAssociativeCollectionType {
-    internal typealias Element = (Key, Value)
-    internal typealias Index = Int
-    internal typealias Generator = IndexingGenerator<SortedArray<Key, Value>>
+public struct SortedArray<Key: Comparable, Value>: SortedAssociativeCollectionType {
+    public typealias Element = (Key, Value)
+    public typealias Index = Int
+    public typealias Generator = IndexingGenerator<SortedArray<Key, Value>>
 
     private var contents: ContiguousArray<Element>
 
-    internal init() {
+    public init() {
         contents = []
     }
-    internal init(minimumCapacity: Int) {
+    public init(minimumCapacity: Int) {
         contents = []
         contents.reserveCapacity(minimumCapacity)
     }
 
-    internal var count: Int { return contents.count }
-    internal var startIndex: Int { return 0 }
-    internal var endIndex: Int { return count }
+    public var count: Int { return contents.count }
+    public var startIndex: Int { return 0 }
+    public var endIndex: Int { return count }
 
-    internal subscript(index: Int) -> Element {
+    public mutating func reserveCapacity(minimumCapacity: Int) {
+        contents.reserveCapacity(minimumCapacity)
+    }
+    
+    public subscript(index: Int) -> Element {
         return contents[index]
     }
 
-    internal subscript(key: Key) -> Value? {
+    public subscript(key: Key) -> Value? {
         get {
             let (value, _) = slotOf(key)
             return value
@@ -52,14 +56,14 @@ internal struct SortedArray<Key: Comparable, Value>: SortedAssociativeCollection
         }
     }
 
-    internal func indexForKey(key: Key) -> Index? {
+    public func indexForKey(key: Key) -> Index? {
         switch slotOf(key) {
         case (nil, _): return nil
         case (.Some(_), let index): return index
         }
     }
 
-    internal mutating func updateValue(value: Value, forKey key: Key) -> Value? {
+    public mutating func updateValue(value: Value, forKey key: Key) -> Value? {
         let (old, index) = slotOf(key)
         if old != nil {
             contents[index] = (key, value)
@@ -71,11 +75,11 @@ internal struct SortedArray<Key: Comparable, Value>: SortedAssociativeCollection
         }
     }
 
-    internal mutating func removeAtIndex(index: Index) -> (Key, Value) {
+    public mutating func removeAtIndex(index: Index) -> (Key, Value) {
         return contents.removeAtIndex(index)
     }
 
-    internal mutating func removeValueForKey(key: Key) -> Value? {
+    public mutating func removeValueForKey(key: Key) -> Value? {
         let (old, index) = slotOf(key)
         if old != nil {
             contents.removeAtIndex(index)
@@ -86,7 +90,7 @@ internal struct SortedArray<Key: Comparable, Value>: SortedAssociativeCollection
         }
     }
 
-    internal mutating func removeAll() {
+    public mutating func removeAll() {
         contents.removeAll()
     }
 
