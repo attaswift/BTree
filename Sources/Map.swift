dfef61543620aabb7770e257f9839d279718cd23
Karoly Lorentey <karoly@lorentey.hu>
2016-01-23 14:32:39 +0100

    Convert Map to use BTree for storage.

--- Sources/Map.swift
+++ Sources/Map.swift
@@ -10,13 +10,9 @@ import Foundation
 
 public struct Map<Key: Comparable, Value>: SortedAssociativeCollectionType {
     // Typealiases
+    internal typealias Tree = BTree<Key, Value>
 
-    internal typealias Summary = EmptySummary<Key>
-    internal typealias TreeKey = StoredKey<Summary>
-    internal typealias Tree = RedBlackTree<TreeKey, Value>
-    internal typealias Handle = Tree.Handle
-
-    public typealias Index = MapIndex<Key, Value>
+    public typealias Index = TreeIndex<Key, Value>
     public typealias Generator = MapGenerator<Key, Value>
     public typealias Element = (Key, Value)
 
@@ -25,19 +21,17 @@ public struct Map<Key: Comparable, Value>: SortedAssociativeCollectionType {
     internal private(set) var tree: Tree
 
     // Initalizer 
-
     public init() {
         self.tree = Tree()
     }
 
     // Properties
-
     public var startIndex: Index {
-        return Index(tree: tree, handle: tree.leftmost)
+        return tree.startIndex
     }
 
     public var endIndex: Index {
-        return Index(tree: tree, handle: nil)
+        return tree.endIndex
     }
 
     public var count: Int {
@@ -47,21 +41,19 @@ public struct Map<Key: Comparable, Value>: SortedAssociativeCollectionType {
     // Subscripts
 
     public subscript(index: Index) -> Element {
-        let (key, payload) = tree.elementAt(index.handle!)
-        return (key.head, payload)
+        return tree[index]
     }
 
     public subscript(key: Key) -> Value? {
         get {
-            guard let handle = tree.find(TreeKey(key)) else { return nil }
-            return tree.payloadAt(handle)
+            return tree.payloadOf(key)
         }
         set(value) {
             if let value = value {
-                tree.setPayloadOf(TreeKey(key), to: value)
+                tree.set(key, to: value)
             }
-            else if let handle = tree.find(TreeKey(key)) {
-                tree.remove(handle)
+            else {
+                tree.remove(key)
             }
         }
     }
@@ -69,34 +61,31 @@ public struct Map<Key: Comparable, Value>: SortedAssociativeCollectionType {
     // Methods
 
     public func generate() -> Generator {
-        return Generator(tree: tree)
+        return Generator(tree.generate())
+    }
+
+    public func _copyToNativeArrayBuffer() -> _ContiguousArrayBuffer<Element> {
+        // The comment in BTree._copyToNativeArrayBuffer explains what this is.
+        return tree._copyToNativeArrayBuffer()
     }
 
     public func indexForKey(key: Key) -> Index? {
-        guard let handle = tree.find(TreeKey(key)) else { return nil }
-        return Index(tree: tree, handle: handle)
+        guard let i = tree.indexOf(key) else { return nil }
+        return Index(i)
     }
 
     // Mutators
 
-    public mutating func reserveCapacity(minimumCapacity: Int) {
-        tree.reserveCapacity(minimumCapacity)
-    }
-
     public mutating func updateValue(value: Value, forKey key: Key) -> Value? {
-        return tree.setPayloadOf(TreeKey(key), to: value).1
+        return tree.set(key, to: value)
     }
 
     public mutating func removeAtIndex(index: Index) -> (Key, Value) {
-        let handle = index.handle!
-        let key = tree.headAt(handle)
-        let value = tree.remove(handle)
-        return (key, value)
+        return tree.removeAt(index)
     }
 
     public mutating func removeValueForKey(key: Key) -> Value? {
-        guard let handle = tree.find(TreeKey(key)) else { return nil }
-        return tree.remove(handle)
+        return tree.remove(key)
     }
 
     public mutating func removeAll() {
@@ -104,59 +93,17 @@ public struct Map<Key: Comparable, Value>: SortedAssociativeCollectionType {
     }
 }
 
-public struct MapIndex<Key: Comparable, Value>: BidirectionalIndexType {
-    private typealias Summary = EmptySummary<Key>
-    private typealias TreeKey = StoredKey<Summary>
-    private typealias Tree = RedBlackTree<TreeKey, Value>
-    private typealias Handle = Tree.Handle
-
-    private let tree: Tree
-    private let handle: Handle?
-
-    private init(tree: Tree, handle: Handle?) {
-        self.tree = tree
-        self.handle = handle
-    }
-
-    public func successor() -> MapIndex<Key, Value> {
-        return MapIndex(tree: tree, handle: tree.successor(handle!))
-    }
-
-    public func predecessor() -> MapIndex<Key, Value> {
-        return MapIndex(tree: tree, handle: tree.predecessor(handle!))
-    }
-}
-
-public func ==<Key: Comparable, Value>(a: MapIndex<Key, Value>, b: MapIndex<Key, Value>) -> Bool {
-    return a.handle == b.handle
-}
-
 public struct MapGenerator<Key: Comparable, Value>: GeneratorType {
     public typealias Element = (Key, Value)
 
-    private typealias Summary = EmptySummary<Key>
-    private typealias TreeKey = StoredKey<Summary>
-    private typealias Tree = RedBlackTree<TreeKey, Value>
-    private typealias Handle = Tree.Handle
+    private var generator: BTreeGenerator<Key, Value>
 
-    private let tree: Tree
-    private var handle: Handle?
-
-    private init(tree: Tree) {
-        self.tree = tree
-        self.handle = tree.leftmost
-    }
-
-    private init(tree: Tree, handle: Handle?) {
-        self.tree = tree
-        self.handle = handle
+    private init(_ generator: BTreeGenerator<Key, Value>) {
+        self.generator = generator
     }
 
     public mutating func next() -> Element? {
-        guard let handle = handle else { return nil }
-        self.handle = tree.successor(handle)
-        let (key, payload) = tree.elementAt(handle)
-        return (key.head, payload)
+        return generator.next()
     }
 }
 
