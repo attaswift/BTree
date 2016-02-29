2e2455427703de6da3fce10056814b1f0c485bbe
Karoly Lorentey <karoly@lorentey.hu>
2016-02-29 11:41:03 +0100

    Add support for adding entire nodes in the tree builder.
    One baby step toward b-tree merging.

--- Sources/BTreeBuilder.swift
+++ Sources/BTreeBuilder.swift
@@ -26,6 +26,7 @@ private enum BuilderState {
 internal struct BTreeBuilder<Key: Comparable, Payload> {
     typealias Node = BTreeNode<Key, Payload>
     typealias Element = Node.Element
+    typealias Splinter = Node.Splinter
 
     private let order: Int
     private let keysPerNode: Int
@@ -54,40 +55,123 @@ internal struct BTreeBuilder<Key: Comparable, Payload> {
         case .Element:
             seedling.append(element)
             if seedling.count == keysPerNode {
-                finishSeedling()
+                closeSeedling()
                 state = .Separator
             }
         }
     }
 
-    private mutating func finishSeedling() {
-        // Append seedling into saplings, combining the last few seedlings when possible.
-        while !saplings.isEmpty && seedling.elements.count == keysPerNode {
-            let sapling = saplings.last!
-            assert(sapling.depth >= seedling.depth)
-            if sapling.depth == seedling.depth + 1 && sapling.elements.count < keysPerNode {
-                // Graft current seedling under the last sapling, as a new child branch.
-                saplings.removeLast()
-                let separator = separators.removeLast()
-                sapling.elements.append(separator)
-                sapling.children.append(seedling)
-                sapling.count += seedling.count + 1
-                seedling = sapling
-            }
-            else if sapling.depth == seedling.depth && sapling.elements.count == keysPerNode {
-                // We have two full nodes; add them as two branches of a new, deeper seedling.
-                saplings.removeLast()
-                let separator = separators.removeLast()
-                seedling = Node(left: sapling, separator: separator, right: seedling)
-            }
-            else {
-                break
-            }
-        }
-        saplings.append(seedling)
+    private mutating func closeSeedling() {
+        appendSapling(seedling)
         seedling = Node(order: order)
     }
 
+    mutating func append(node: Node) {
+        appendWithoutCloning(node.clone())
+    }
+
+    mutating func appendWithoutCloning(node: Node) {
+        assert(node.order == order)
+        if node.depth == 0 {
+            if node.isEmpty { return }
+            if state == .Separator {
+                assert(seedling.isEmpty)
+                separators.append(node.elements.removeFirst())
+                node.count -= 1
+                state = .Element
+                if node.isEmpty { return }
+                seedling = node
+            }
+            else if seedling.count > 0 {
+                let sep = seedling.elements.removeLast()
+                seedling.count -= 1
+                if let splinter = seedling.shiftSlots(separator: sep, node: node, target: keysPerNode) {
+                    closeSeedling()
+                    separators.append(splinter.separator)
+                    seedling = splinter.node
+                }
+            }
+            else {
+                seedling = node
+            }
+            if seedling.count >= keysPerNode {
+                closeSeedling()
+                state = .Separator
+            }
+            return
+        }
+
+        if state == .Element && seedling.count > 0 {
+            let sep = seedling.elements.removeLast()
+            seedling.count -= 1
+            closeSeedling()
+            separators.append(sep)
+        }
+        if state == .Separator {
+            let cursor = BTreeCursor(BTreeCursorPath(endOf: saplings.removeLast()))
+            cursor.moveBackward()
+            let separator = cursor.remove()
+            saplings.append(cursor.finish())
+            separators.append(separator)
+        }
+        assert(seedling.isEmpty)
+        appendSapling(node)
+        state = .Separator
+    }
+
+    private mutating func appendSapling(sapling: Node) {
+        var sapling = sapling
+        outer: while !saplings.isEmpty {
+            assert(saplings.count == separators.count)
+            var previous = saplings.removeLast()
+            let separator = separators.removeLast()
+
+            // Join previous saplings together until they grow at least as deep as the new one.
+            while previous.depth < sapling.depth {
+                if saplings.isEmpty {
+                    // If the single remaining sapling is too shallow, just join it to the new sapling and call it a day.
+                    sapling = Node.join(left: previous, separator: separator, right: sapling)
+                    break outer
+                }
+                previous = Node.join(left: saplings.removeLast(), separator: separators.removeLast(), right: previous)
+            }
+
+            let fullPrevious = previous.elements.count >= keysPerNode
+            let fullSapling = sapling.elements.count >= keysPerNode
+
+            if previous.depth == sapling.depth + 1 && !fullPrevious && fullSapling {
+                // Graft node under the last sapling, as a new child branch.
+                previous.elements.append(separator)
+                previous.children.append(sapling)
+                previous.count += sapling.count + 1
+                sapling = previous
+            }
+            else if previous.depth == sapling.depth && fullPrevious && fullSapling {
+                // We have two full nodes; add them as two branches of a new, deeper node.
+                sapling = Node(left: previous, separator: separator, right: sapling)
+            }
+            else if previous.depth > sapling.depth || fullPrevious {
+                // The new sapling can be appended to the line and we're done.
+                saplings.append(previous)
+                separators.append(separator)
+                break
+            }
+            else if let splinter = previous.shiftSlots(separator: separator, node: sapling, target: keysPerNode) {
+                // We have made the previous sapling full; add it as a new one before trying again with the remainder.
+                assert(previous.elements.count == keysPerNode)
+                appendSapling(previous)
+                separators.append(splinter.separator)
+                sapling = splinter.node
+            }
+            else {
+                // We've combined the two saplings; try again with the result.
+                sapling = previous
+            }
+        }
+        saplings.append(sapling)
+    }
+
+    @warn_unused_result
     mutating func finish() -> Node {
         // Merge all saplings and the seedling into a single tree.
         var root: Node
@@ -102,6 +186,7 @@ internal struct BTreeBuilder<Key: Comparable, Payload> {
         while !saplings.isEmpty {
             root = Node.join(left: saplings.removeLast(), separator: separators.removeLast(), right: root)
         }
+        state = .Element
         return root
     }
 }
---
+++ Tests/BTreeBuilderTests.swift
@@ -0,0 +1,201 @@
+//
+//  BTreeBuilderTests.swift
+//  BTree
+//
+//  Created by Károly Lőrentey on 2016-02-29.
+//  Copyright © 2016 Károly Lőrentey.
+//
+
+import XCTest
+@testable import BTree
+
+class BTreeBuilderTests: XCTestCase {
+    typealias Builder = BTreeBuilder<Int, String>
+    typealias Node = BTreeNode<Int, String>
+    typealias Tree = BTree<Int, String>
+    typealias Element = (Int, String)
+
+    func elements(range: Range<Int>) -> [Element] {
+        return range.map { ($0, String($0)) }
+    }
+
+    func testEmpty() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        let tree = builder.finish()
+        tree.assertValid()
+        assert(tree.order == 5)
+        XCTAssertElementsEqual(tree, [])
+    }
+
+    func testSingleElement() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        builder.append((0, "0"))
+        let tree = builder.finish()
+        tree.assertValid()
+        XCTAssertElementsEqual(tree, [(0, "0")])
+    }
+
+    func testFullNodeWithDepth0() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        builder.append((0, "0"))
+        builder.append((1, "1"))
+        builder.append((2, "2"))
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, [(0, "0"), (1, "1"), (2, "2")])
+        XCTAssertEqual(node.depth, 0)
+    }
+
+    func testFullNodeWithDepth1() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 15 {
+            builder.append((i, String(i)))
+        }
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 15).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 1)
+    }
+
+    func testFullNodeWithDepth2() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 63 {
+            builder.append((i, String(i)))
+        }
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testPartialNodeWithDepth2() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 60 {
+            builder.append((i, String(i)))
+        }
+        let node = builder.finish()
+        node.assertValid()
+        XCTAssertElementsEqual(node, (0 ..< 60).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingEmptyNodes() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 63 {
+            builder.append(Node(order: 5))
+            builder.append((i, String(i)))
+        }
+        builder.append(Node(order: 5))
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingSingleElementNodes() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 63 {
+            let node = Node(order: 5, elements: [(i, String(i))], children: [], count: 1)
+            builder.append(node)
+        }
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingTwoElementNodes() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 31 {
+            let node = Node(order: 5, elements: elements(2 * i ..< 2 * i + 2), children: [], count: 2)
+            builder.append(node)
+        }
+        builder.append((62, "62"))
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingThreeElementNodes() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 21 {
+            let node = Node(order: 5, elements: elements(3 * i ..< 3 * i + 3), children: [], count: 3)
+            builder.append(node)
+        }
+        let node = builder.finish()
+        node.assertValid()
+        node.forEachNode { XCTAssertEqual($0.elements.count, 3) }
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingFourElementNodes() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        for i in 0 ..< 15 {
+            let node = Node(order: 5, elements: elements(4 * i ..< 4 * i + 4), children: [], count: 4)
+            builder.append(node)
+        }
+        builder.append((60, "60"))
+        builder.append((61, "61"))
+        builder.append((62, "62"))
+        let node = builder.finish()
+        node.assertValid()
+        XCTAssertElementsEqual(node, (0 ..< 63).map { ($0, String($0)) })
+        XCTAssertEqual(node.depth, 2)
+    }
+
+    func testAppendingNodesWithDepth1() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        var i = 0
+        for _ in 0 ..< 5 {
+            let node = maximalNode(depth: 1, order: 5, offset: i)
+            print(node.map { $0.0 })
+            i += node.count
+            builder.append(node)
+        }
+        let node = builder.finish()
+        node.assertValid()
+        XCTAssertElementsEqual(node.map { $0.0 }, 0 ..< i)
+    }
+
+    func testAppendingOneElementThenANodeWithDepth1() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        var i = 0
+        for _ in 0 ..< 4 {
+            builder.append((i, String(i)))
+            i += 1
+            let node = minimalNode(depth: 1, order: 5, offset: i)
+            print(node.map { $0.0 })
+            i += node.count
+            builder.append(node)
+        }
+        let node = builder.finish()
+        node.assertValid()
+        XCTAssertElementsEqual(node.map { $0.0 }, 0 ..< i)
+    }
+
+    func testAppendingTwoElementsThenANodeWithDepth1() {
+        var builder = Builder(order: 5, keysPerNode: 3)
+        var i = 0
+        for _ in 0 ..< 4 {
+            builder.append((i, String(i)))
+            builder.append((i + 1, String(i + 1)))
+            i += 2
+            let node = minimalNode(depth: 1, order: 5, offset: i)
+            print(node.map { $0.0 })
+            i += node.count
+            builder.append(node)
+        }
+        let node = builder.finish()
+        node.assertValid()
+        XCTAssertElementsEqual(node.map { $0.0 }, 0 ..< i)
+    }
+
+}
