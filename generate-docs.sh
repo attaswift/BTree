d8bb556fd4fa107d48079ce2d6aaf70936b479b6
Karoly Lorentey <karoly@lorentey.hu>
2016-03-02 16:27:48 +0100

    Allow doc generation for unreleased snapshots.

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 module="BTree"
 scheme="BTree-Mac"
 
 version="$(grep VERSION_STRING version.xcconfig | sed 's/^VERSION_STRING = //' | sed 's/ *$//')"
-tag="v$version"
+case "$version" in
+*-dev)
+  # For dev versions, use the current revision.
+  ref="$(git rev-parse HEAD)"
+  ;;
+*)
+  # For releases, use the tagged commit.
+  ref="v$version"
+  ;;
+esac
 
 jazzy \
     --clean \
     --author "Károly Lőrentey" \
     --author_url "https://twitter.com/lorentey" \
     --github_url "https://github.com/lorentey/$module" \
-    --github-file-prefix "https://github.com/lorentey/$module/tree/$tag" \
+    --github-file-prefix "https://github.com/lorentey/$module/tree/$ref" \
     --module-version "$version" \
     --xcodebuild-arguments "-scheme,$scheme" \
     --module "$module" \
