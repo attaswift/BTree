98ee643804911088df70df689b556e5353a47e96
Károly Lőrentey <karoly@lorentey.hu>
2017-08-05 13:20:39 +0200

    Upgrade to SPM 4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
+// swift-tools-version:4.0
 //
 //  Package.swift
 //  BTree
 //
 //  Created by Károly Lőrentey on 2015-12-15.
-//  Copyright © 2015–2016 Károly Lőrentey.
+//  Copyright © 2015-2017 Károly Lőrentey.
 //
 
 import PackageDescription
 
 let package = Package(
     name: "BTree",
-    dependencies: []
+    products: [
+        .library(name: "BTree", type: .dynamic, targets: ["BTree"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "BTree", dependencies: [], path: "Sources"),
+        .testTarget(name: "BTreeTests", dependencies: ["BTree"], path: "Tests/BTreeTests")
+    ],
+    swiftLanguageVersions: [4]
 )
